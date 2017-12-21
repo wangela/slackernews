ab92556c65f75fe6cfa1c28fa30f5868c61d9a34
wangela <wangela@users.noreply.github.com>
2017-12-21 00:56:53 -1000

    Debug python script

--- all-time-popular.py
+++ all-time-popular.py
@@ -1,6 +1,5 @@
 import psycopg2
 
-DBNAME = "news"
 # Check Python code quality using pep8 command line tool
 # Refer to PEP8 style guide at https://www.python.org/dev/peps/pep-0008/
 # Document SQL views in the README
@@ -9,12 +8,12 @@ DBNAME = "news"
 # Presents the top 3 articles with the most popular article at top
 # Popularity defined by number of page views over all time
 def top_articles():
-    db = psycopg2.connect("dbname=DBNAME")
+    db = psycopg2.connect("dbname=news")
     curr = db.cursor()
-    curr.execute("SELECT title, count(*) AS views FROM articles_log
+    curr.execute("""SELECT title, count(*) AS views FROM articles_log
         WHERE status != '404 NOT FOUND'
         GROUP BY title
-        ORDER BY views DESC LIMIT 3;")
+        ORDER BY views DESC LIMIT 3;""")
     print ("TOP ARTICLES:")
     return curr.fetchall()
     curr.close()
@@ -24,11 +23,11 @@ def top_articles():
 # Presents the authors with the most popular authors at top
 # Popularity defined by sum of author's articles page views over all time
 def top_authors():
-    db = psycopg2.connect("dbname=DBNAME")
+    db = psycopg2.connect("dbname=news")
     curr = db.cursor()
-    curr.execute("SELECT name, count(*) AS views FROM articles_log
+    curr.execute("""SELECT name, count(*) AS views FROM articles_log
         WHERE status != '404 NOT FOUND'
-        GROUP BY name ORDER BY views DESC;")
+        GROUP BY name ORDER BY views DESC;""")
     print ("TOP AUTHORS:")
     return curr.fetchall()
     curr.close()
@@ -38,21 +37,21 @@ def top_authors():
 # Presents all the days where more than 1% of requests
 #   led to HTTP status errors
 def buggy_days():
-    db = psycopg2.connect("dbname=DBNAME")
+    db = psycopg2.connect("dbname=news")
     curr = db.cursor()
-    curr.execute("SELECT date, requests, errors, percentage
+    curr.execute("""SELECT date, requests, errors, percentage
         FROM (SELECT date, requests, errors,
             trunc(100.0*errors / requests, 2) AS percentage
             FROM requests LEFT JOIN errors
             USING (date)) AS subq
         WHERE percentage > 1.00
-        ORDER BY percentage DESC;")
-    print ("HIGH ERROR DAYS")
+        ORDER BY percentage DESC;""")
+    print ("HIGH ERROR DAYS:")
     return curr.fetchall()
     curr.close()
     db.close()
 
 
-top_articles()
-top_authors()
-buggy_days()
+print (top_articles())
+print (top_authors())
+print (buggy_days())
