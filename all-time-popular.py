d40114db241cd9af786b16d0897c454dd64abb1e
wangela <wangela@users.noreply.github.com>
2017-12-21 01:01:15 -1000

    Clean up after check for complaince with PEP 8 Python style guide

--- all-time-popular.py
+++ all-time-popular.py
@@ -1,42 +1,41 @@
 import psycopg2
 
-# Check Python code quality using pep8 command line tool
-# Refer to PEP8 style guide at https://www.python.org/dev/peps/pep-0008/
-# Document SQL views in the README
 
-# Top 3 most popular articles
-# Presents the top 3 articles with the most popular article at top
-# Popularity defined by number of page views over all time
 def top_articles():
+    # Top 3 most popular articles
+    # Presents the top 3 articles with the most popular article at top
+    # Popularity defined by number of page views over all time
     db = psycopg2.connect("dbname=news")
     curr = db.cursor()
     curr.execute("""SELECT title, count(*) AS views FROM articles_log
         WHERE status != '404 NOT FOUND'
         GROUP BY title
         ORDER BY views DESC LIMIT 3;""")
-    print ("TOP ARTICLES:")
+    print("TOP ARTICLES:")
     return curr.fetchall()
     curr.close()
     db.close()
 
-# Most popular article authors
-# Presents the authors with the most popular authors at top
-# Popularity defined by sum of author's articles page views over all time
+
 def top_authors():
+    # Most popular article authors
+    # Presents the authors with the most popular authors at top
+    # Popularity defined by sum of author's articles page views over all time
     db = psycopg2.connect("dbname=news")
     curr = db.cursor()
     curr.execute("""SELECT name, count(*) AS views FROM articles_log
         WHERE status != '404 NOT FOUND'
         GROUP BY name ORDER BY views DESC;""")
-    print ("TOP AUTHORS:")
+    print("TOP AUTHORS:")
     return curr.fetchall()
     curr.close()
     db.close()
 
-# Buggy Days
-# Presents all the days where more than 1% of requests
-#   led to HTTP status errors
+
 def buggy_days():
+    # Buggy Days
+    # Presents all the days where more than 1% of requests
+    #   led to HTTP status errors
     db = psycopg2.connect("dbname=news")
     curr = db.cursor()
     curr.execute("""SELECT date, requests, errors, percentage
@@ -46,12 +45,12 @@ def buggy_days():
             USING (date)) AS subq
         WHERE percentage > 1.00
         ORDER BY percentage DESC;""")
-    print ("HIGH ERROR DAYS:")
+    print("HIGH ERROR DAYS:")
     return curr.fetchall()
     curr.close()
     db.close()
 
 
-print (top_articles())
-print (top_authors())
-print (buggy_days())
+print(top_articles())
+print(top_authors())
+print(buggy_days())
